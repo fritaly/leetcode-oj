4dd80994a49a3bcfb3630e9fc1b17ba14df1dbc4
Francois Ritaly <francois.ritaly@gmail.com>
2015-01-20 19:54:08 +0100

    Added solution to problem "Majority Element"

---
+++ src/main/java/com/github/fritaly/leetcode/MajorityElement.java
@@ -0,0 +1,13 @@
+package com.github.fritaly.leetcode;
+
+import java.util.Arrays;
+
+public class MajorityElement {
+
+    public int majorityElement(int[] num) {
+    	Arrays.sort(num);
+
+    	// The middle element is necessarily the majority element
+    	return num[num.length / 2];
+    }
+}
