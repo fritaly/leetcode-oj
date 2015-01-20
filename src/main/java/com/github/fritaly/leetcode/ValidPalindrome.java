b782ad91bd47c267dd19d4ca6c54768852b48981
Francois Ritaly <francois.ritaly@gmail.com>
2015-01-20 20:09:52 +0100

    Added solution to problem "Valid Palindrome"

---
+++ src/main/java/com/github/fritaly/leetcode/ValidPalindrome.java
@@ -0,0 +1,14 @@
+package com.github.fritaly.leetcode;
+
+public class ValidPalindrome {
+
+	private static String reverse(String string) {
+		return new StringBuilder(string).reverse().toString();
+	}
+
+    public boolean isPalindrome(String s) {
+    	final String string = s.replaceAll("[^a-zA-Z0-9]+", "");
+
+    	return string.equalsIgnoreCase(reverse(string));
+    }
+}
