a1fc230352d314c0f684df337949cd5649717b2a
Francois Ritaly <francois.ritaly@gmail.com>
2015-01-20 20:06:05 +0100

    Added solution to problem "Palindrome Number"

---
+++ src/main/java/com/github/fritaly/leetcode/PalindromeNumber.java
@@ -0,0 +1,14 @@
+package com.github.fritaly.leetcode;
+
+public class PalindromeNumber {
+
+	private static String reverse(String string) {
+		return new StringBuilder(string).reverse().toString();
+	}
+
+    public boolean isPalindrome(int x) {
+    	final String text = Integer.toString(x);
+
+		return text.equals(reverse(text));
+    }
+}
